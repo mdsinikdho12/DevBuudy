d2f1921a5ac1951438d71bdc58e3ec3c4bad8230
md sinikdho <mdsinikdho94@gmail.com>
2026-03-22 22:54:04 +0600

    Add Keybord shortcut

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+
+// Alt+D side panel open
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "open-devbuddy") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.sidePanel.open({ tabId: tabs[0].id });
+    });
+  }
+});
